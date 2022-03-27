ab87d649ad08fcccb8dc9d84e04beea5b139d1b8
Juanvcas <juanvcas96@gmail.com>
2022-03-27 11:26:55 -0500

    Configured Babel loader and its scripts and the HTML loader and its plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require("path");
     // Este es un modulo que ya viene incluido con webpack.
 
+//Plugins
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+    // Instanciamos el plugin para poder configurarlo en webpack.
+
 //Configuración del proyecto
 module.exports = {
     entry: "./src/index.js",
@@ -14,5 +18,35 @@ module.exports = {
     resolve: {
         extensions: [".js"]
             // Con que extensiones va a trabajar, se deben de poner todas con las que va a compilar el royecto, si utiliza ya sea react, svelt u otro framework u otro lenguaje de programación, se debe de establecer aqui.
-    }
+    },
+
+    //Herramientas y loaders
+    module: {
+        // Destro de module, implementamos las reglas de nuestro proyecto, que van a estar en el apartado "rules" y en un array de objetos con estas reglas.
+        rules: [
+            {
+                test: /\.m?js$/,
+                    // Esto es una expresión regular, que lo que hace es establecer con que tipos de archivos se va a trabajar, empieza "/" ya sea cualquier "\." .m o "?" cualquier .js, y la expresión termina con "$/".
+                exclude: /node_modules/,
+                    // Este apartado es para excluir carpetas del proyecto que no se necesitan leer. (IMPORTANTE)
+                use: {
+                    // Aqui, configuramos que loaders se van a utilizar en nuestro proyecto.
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    plugins: [
+        // Aqui se configuran los plugins que va a utilizar webpack, y se instancian con "new" y la variable que contiene al plugin.
+        new HtmlWebpackPlugin({
+            // Configuramos los parametros que necesitamos.
+            inject: true,
+                // Este apartado indica que se hara una incersión del script (enlace) dentro del archivo html.
+                    //* Se indica "true" para insertarlo ya sea en el body o el head, "body" para incertarlo directamente en el body, "head" para insertarlo directamente en el head, y "false" para que no se haga una inserción.
+            template: "./public/index.html",
+                // Este apartado nos sirve para indicar un template que va a utilizar el proyecto.
+            filename: "index.html"
+                // Este partado nos sirve para indicar el nombre del archivo resultante de la compilación de HTML.
+        })
+    ]
 }
